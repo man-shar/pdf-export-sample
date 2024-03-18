3ab67975cbb954ea470a9bfd0a31a15b46c25baa
Manas Sharma <manasdotsharma@gmail.com>
2024-03-18 22:58:10 +0530

    use pdfmake instead

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,18 @@ import "./pdfkit.standalone";
 import "./blob-stream";
 import SVGtoPDF from "svg-to-pdfkit";
 
+import { createPdf } from "pdfmake";
+
+import "./vfs_fonts";
+
+console.log(createPdf);
+
+var fonts = {
+  Inter: {
+    normal: "Inter-regular.ttf",
+  },
+};
+
 PDFDocument.prototype.addSVG = function (svg, x, y, options) {
   return SVGtoPDF(this, svg, x, y, options), this;
 };
@@ -14,7 +26,7 @@ const pageMargin = {
   bottom: 50,
 };
 
-export async function createPDF(chartCtr) {
+export async function _createPDF(chartCtr) {
   console.log(chartCtr);
   const doc = new PDFDocument({
     pdfVersion: "1.5",
@@ -97,6 +109,81 @@ export async function createPDF(chartCtr) {
   stream.on("finish", function () {
     // get a blob you can do whatever you like with
     const blob = stream.toBlob("application/pdf");
+    // download to file
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "example.pdf";
+    a.click();
+  });
+}
+
+export function createPDF(chartCtr) {
+  // get svg string
+  const svgStr = chartCtr.querySelector("svg").outerHTML;
+  const svg = chartCtr.querySelector("svg");
+
+  //   get title and subtitle .headline and .subtitle with styles
+  const title = chartCtr.querySelector(".headline");
+  const subtitle = chartCtr.querySelector(".subtitle");
+
+  // get the svg, headline and subtitles' x and y client rects relative to the chartCtr
+  const svgRect = svg.getBoundingClientRect();
+  const titleRect = title.getBoundingClientRect();
+  const subtitleRect = subtitle.getBoundingClientRect();
+  const chartRect = chartCtr.getBoundingClientRect();
+
+  // get the svg, headline and subtitles' x and y client rects relative to the chartCtr
+  const svgX = svgRect.x - chartRect.x + pageMargin.left;
+  const svgY = svgRect.y - chartRect.y + pageMargin.top;
+  const titleX = titleRect.x - chartRect.x + pageMargin.left;
+  const titleY = titleRect.y - chartRect.y + pageMargin.top;
+  const subtitleX = subtitleRect.x - chartRect.x + pageMargin.left;
+  const subtitleY = subtitleRect.y - chartRect.y + pageMargin.top;
+
+  //   get font sizes computed
+  const titleFontSize = +window
+    .getComputedStyle(title)
+    .fontSize.replace("px", "");
+  const subtitleFontSize = +window
+    .getComputedStyle(subtitle)
+    .fontSize.replace("px", "");
+
+  var docDefinition = {
+    defaultStyle: {
+      font: "Inter",
+    },
+    content: [
+      {
+        text: title.textContent,
+        style: "header",
+      },
+      {
+        svg: svg.outerHTML,
+        width: 600,
+        height: 400,
+        css: true,
+      },
+      {
+        text: subtitle.textContent,
+        style: "subheader",
+      },
+    ],
+    styles: {
+      header: {
+        font: "Inter",
+        fontSize: titleFontSize,
+      },
+      subheader: {
+        font: "Inter",
+        fontSize: subtitleFontSize,
+      },
+    },
+  };
+
+  var doc = createPdf(docDefinition, null, fonts);
+
+  doc.getBlob((blob) => {
     // download to file
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
